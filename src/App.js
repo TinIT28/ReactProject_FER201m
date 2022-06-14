a88038bb234f3f8ca1361bcc1898b8a0b0248621
TinIT28 <tinmcit@gmail.com>
2022-06-14 07:52:32 +0700

    Lab 5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Thức Bùi Bùi
         </a>
       </header>
     </div>
