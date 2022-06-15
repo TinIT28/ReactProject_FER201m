a673735fef5ba3f8e36c01e6a77fc8be8302b2ea
TinIT28 <tinmcit@gmail.com>
2022-06-15 09:16:49 +0700

    Lab 5 React Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, {Component} from "react";
-import Menu from './components/MenuComponent.js';
+import DishDetail from "./components/DishDetailComponent.js";
+import { DISHES } from './shared/dishes.js';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+  };
+}
+
   render() {
     return (
-      <Menu />
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <DishDetail dishes={this.state.dishes} />
+      </div>
     );
   }
 }
