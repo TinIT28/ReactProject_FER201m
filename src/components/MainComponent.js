2479e358d3e4820c56d359b1bc5cf99c27e12a07
TinIT28 <tinmcit@gmail.com>
2022-06-21 09:37:53 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishDetailComponent";
-import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 class Main extends Component {
@@ -13,6 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       selectedDish: null,
     };
   }
@@ -26,13 +33,23 @@ class Main extends Component {
       <div>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route
-            path="/menu"
-            element={ <Menu dishes={this.state.dishes} />}
+            path="/home"
+            element={
+              <Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={
+                  this.state.promotions.filter((promo) => promo.featured)[0]
+                }
+                leader={
+                  this.state.leaders.filter((leader) => leader.featured)[0]
+                }
+              />
+            }
           />
-          
-          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
+          <Route path="/contactus" element={<Contact />} />
+          <Route path="/" element={<Navigate to="/home" />} />
         </Routes>
         <Footer />
       </div>
