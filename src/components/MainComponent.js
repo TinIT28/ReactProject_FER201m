65c09631d2725fda88438a04705df45bffaee2ae
TinIT28 <tinmcit@gmail.com>
2022-06-21 09:46:49 +0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,6 +12,13 @@ import { PROMOTIONS } from "../shared/promotions";
 import { LEADERS } from "../shared/leaders";
 import { Routes, Route, Navigate } from "react-router-dom";
 
+const DishWithId = ({match}) => {
+    return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    );
+  };
+
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -27,6 +34,7 @@ class Main extends Component {
   onDishSelect(dishId) {
     this.setState({ selectedDish: dishId });
   }
+  
 
   render() {
     return (
@@ -50,6 +58,7 @@ class Main extends Component {
           <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
           <Route path="/contactus" element={<Contact />} />
           <Route path="/" element={<Navigate to="/home" />} />
+          <Route path='/menu/:dishId' element={DishWithId} />
         </Routes>
         <Footer />
       </div>
