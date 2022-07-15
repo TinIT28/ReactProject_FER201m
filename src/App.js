11b86751ce407b5f59a6b6655d56a1b58aad1737
TinIT28 <tinmcit@gmail.com>
2022-07-15 16:11:02 +0700

    Fetch from Server

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react"
-import { BrowserRouter } from "react-router-dom"
-import { Provider } from "react-redux"
-import store from "./redux/store"
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
-import "./App.css";
-import Main from "./components/MainComponent";
+const store = ConfigureStore();
 
-function App() {
-	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<div>
-					<Main />
-				</div>
-			</BrowserRouter>
-		</Provider>
-	)
+class App extends Component {
+
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
+
 
 export default App;
